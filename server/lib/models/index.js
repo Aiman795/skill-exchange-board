6976c0cb8bba10f34e5fd2c4d6c8fd07e4da6d1d
Fatima Noor Ul Imran <ifatimanoorimran@gmail.com>
2026-07-12 22:37:17 +0500

    Delete server directory

--- server/lib/models/index.js
+++
@@ -1,4 +0,0 @@
-export { default as User } from "./User";
-export { default as Listing } from "./Listing";
-export { default as Match } from "./Match";
-export { default as Message } from "./Message";
