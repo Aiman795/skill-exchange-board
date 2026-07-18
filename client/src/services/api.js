ce6134f1a0769ee78b1b6d618c89ae4b348b7139
Fatima Tul Zahra <fatimatulzahraai22@gmail.com>
2026-07-18 01:17:31 -0700

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://skill-exchange-board-production.up.railway.app/api";
 
 // Create axios instance
 const api = axios.create({
