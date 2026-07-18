36585c4c716fdb41c904b3e01fd7bebae71a28d1
Fatima Tul Zahra <fatimatulzahraai22@gmail.com>
2026-07-18 01:20:08 -0700

    Update socketClient.js

--- client/src/services/socketClient.js
+++ client/src/services/socketClient.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const connectSocket = (token) => {
-  return io("http://localhost:5000", {
+  return io("https://skill-exchange-board-production.up.railway.app", {
     auth: { token },
   });
 };
